db4354a330acb73b8e75b378a85ce75f15f91d11
Kruithne <kruithne@gmail.com>
2013-06-03 20:59:29 +0100

    Implemented HelloWorld function

---
+++ src/no/runsafe/eventengine/functions/HelloWorld.java
@@ -0,0 +1,15 @@
+package no.runsafe.eventengine.functions;
+
+import no.runsafe.framework.server.RunsafeServer;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.ZeroArgFunction;
+
+public class HelloWorld extends ZeroArgFunction
+{
+	@Override
+	public LuaValue call()
+	{
+		RunsafeServer.Instance.broadcastMessage("Hello, world!");
+		return null;
+	}
+}
