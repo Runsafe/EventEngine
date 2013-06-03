55fe3d1733424c0018c8f0818645a96c50764a38
Kruithne <kruithne@gmail.com>
2013-06-03 21:55:30 +0100

    Remove the need to require functions in the Lua scripts

--- src/no/runsafe/eventengine/functions/Player.java
+++ src/no/runsafe/eventengine/functions/Player.java
@@ -2,22 +2,24 @@ package no.runsafe.eventengine.functions;
 
 import no.runsafe.framework.server.RunsafeServer;
 import no.runsafe.framework.server.player.RunsafePlayer;
+import org.luaj.vm2.Globals;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
-import org.luaj.vm2.lib.TwoArgFunction;
 
-public class Player extends TwoArgFunction
+public class Player extends OneArgFunction
 {
 	public Player() {}
 
 	@Override
-	public LuaValue call(LuaValue modName, LuaValue env)
+	public LuaValue call(LuaValue env)
 	{
-		LuaTable player = tableOf();
+		Globals globals = env.checkglobals();
+		LuaTable player = new LuaTable();
 		player.set("kill", new kill());
 
-		env.set("Player", player);
+		env.set("player", player);
+		globals.package_.loaded.set("player", player);
 		return player;
 	}
 
