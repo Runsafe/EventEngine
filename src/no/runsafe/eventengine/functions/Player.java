e3d53a8082f00120cb5acbebe2f10d9c1a2e0cfa
Kruithne <kruithne@gmail.com>
2013-06-03 23:28:41 +0100

    Try this..

--- src/no/runsafe/eventengine/functions/Player.java
+++ src/no/runsafe/eventengine/functions/Player.java
@@ -1,25 +1,21 @@
 package no.runsafe.eventengine.functions;
 
-import no.runsafe.framework.server.RunsafeServer;
-import no.runsafe.framework.server.player.RunsafePlayer;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
-import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.TwoArgFunction;
 import org.luaj.vm2.lib.ZeroArgFunction;
 
-public class Player extends TwoArgFunction
+public class player extends TwoArgFunction
 {
-	public Player() {}
+	public player() {}
 
 	@Override
 	public LuaValue call(LuaValue modName, LuaValue env)
 	{
 		LuaTable player = tableOf();
-		player.set("kill", new kill());
 		player.set("test", new test());
 
-		env.set("Player", player);
+		env.set("player", player);
 		//env.checkglobals().package_.loaded.set("player", player);
 		return player;
 	}
@@ -29,19 +25,7 @@ public class Player extends TwoArgFunction
 		@Override
 		public LuaValue call()
 		{
-			RunsafeServer.Instance.broadcastMessage("Test");
 			return valueOf("Test");
-		}
-	}
-
-	static class kill extends OneArgFunction
-	{
-		@Override
-		public LuaValue call(LuaValue playerName)
-		{
-			RunsafePlayer player = RunsafeServer.Instance.getPlayerExact(playerName.toString());
-			player.setHealth(0);
-			return null;
 		}
 	}
 }
