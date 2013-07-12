5d1846fc33e86ac5406d436773c313308f161bea
Kruithne <kruithne@gmail.com>
2013-07-12 23:43:45 +0100

    Use the passed

--- src/no/runsafe/eventengine/engine/APIHandler.java
+++ src/no/runsafe/eventengine/engine/APIHandler.java
@@ -12,8 +12,8 @@ public class APIHandler extends OneArgFunction
 	public LuaValue call(LuaValue env)
 	{
 		LuaTable lib = new LuaTable();
-		LuaEnvironment.env.set("api", lib);
-		LuaEnvironment.env.get("package").get("loaded").set("api", lib);
+		env.set("api", lib);
+		env.get("package").get("loaded").set("api", lib);
 
 		LuaEnvironment.loadFile("plugins/EventEngine/lua/engine.lua");
 		LuaEnvironment.global.load(new HookingLibrary());
