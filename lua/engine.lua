b07a7b45cb0c0e2bfb3aa9211e98f7bd9a44b13c
Kruithne <kruithne@gmail.com>
2013-07-12 19:42:07 +0100

    No longer require middleclass, automatically injected on framework level.

--- lua/engine.lua
+++ lua/engine.lua
@@ -1,5 +1,3 @@
-require "middleclass"
-
 -- Hook registering
 function registerHook(hookType, functionName, ...)
     engine.hooks.registerHook(hookType, functionName, ...); -- Register the hook with the server.
