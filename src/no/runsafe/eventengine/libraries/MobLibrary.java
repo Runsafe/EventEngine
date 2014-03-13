75995a719e3f41bf7ac042ec868cf2b122542364
Kruithne <kruithne@gmail.com>
2014-03-13 22:53:03 +0000

    Remove the controlled entity stuff, it's rather tacky at this moment in time.

--- src/no/runsafe/eventengine/libraries/MobLibrary.java
+++ src/no/runsafe/eventengine/libraries/MobLibrary.java
@@ -1,19 +1,13 @@
 package no.runsafe.eventengine.libraries;
 
 import net.minecraft.server.v1_7_R1.*;
-import no.runsafe.eventengine.entity.EntityDriver;
-import no.runsafe.eventengine.entity.PathfinderGoToTarget;
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.entity.IEntity;
 import no.runsafe.framework.api.lua.*;
 import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
 import no.runsafe.framework.minecraft.entity.PassiveEntity;
 import org.bukkit.craftbukkit.v1_7_R1.entity.CraftEntity;
-import org.bukkit.craftbukkit.v1_7_R1.util.UnsafeList;
 import org.luaj.vm2.LuaTable;
-
-import java.lang.reflect.Field;
 
 public class MobLibrary extends Library
 {
@@ -47,61 +41,6 @@ public class MobLibrary extends Library
 				ema.k(parameters.getInt(0));
 				ema.l(parameters.getInt(1));
 				return entity.getEntityId();
-			}
-		});
-
-		lib.set("spawnControlledEntity", new IntegerFunction()
-		{
-			@Override
-			public Integer run(FunctionParameters parameters)
-			{
-				IEntity entity = no.runsafe.framework.minecraft.entity.EntityType.getTypeByName(parameters.getString(0)).spawn(parameters.getLocation(1));
-				Entity base = ObjectUnwrapper.getMinecraft(entity);
-
-				if (base instanceof EntityInsentient)
-				{
-					EntityInsentient creature = (EntityCreature) base;
-					try
-					{
-						Field gsa = PathfinderGoalSelector.class.getDeclaredField("b");
-						gsa.setAccessible(true);
-
-						Field gs = EntityInsentient.class.getDeclaredField("goalSelector");
-						Field ts = EntityInsentient.class.getDeclaredField("targetSelector");
-
-						gs.setAccessible(true);
-						ts.setAccessible(true);
-
-						PathfinderGoalSelector goalSelector = (PathfinderGoalSelector) gs.get(creature);
-						PathfinderGoalSelector targetSelector = (PathfinderGoalSelector) ts.get(creature);
-
-						gsa.set(goalSelector, new UnsafeList());
-						gsa.set(targetSelector, new UnsafeList());
-
-						goalSelector.a(1, new PathfinderGoToTarget(creature));
-					}
-					catch (Exception e)
-					{
-						e.printStackTrace();
-					}
-				}
-
-				return entity.getEntityId();
-			}
-		});
-
-		lib.set("goTo", new VoidFunction()
-		{
-			@Override
-			protected void run(FunctionParameters parameters)
-			{
-				ILocation targetLocation = parameters.getLocation(1);
-				IEntity entity = targetLocation.getWorld().getEntityById(parameters.getInt(0));
-
-				if (entity == null)
-					return;
-
-				EntityDriver.addTarget(entity, targetLocation, parameters.getFloat(5));
 			}
 		});
 
