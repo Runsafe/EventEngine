16a9f99a7a02a3bbd52f9d7613bfefac5cd37a4d
Kruithne <kruithne@gmail.com>
2014-03-13 08:25:30 +0000

    Added rough-cut debugging.

--- src/no/runsafe/eventengine/entity/PathfinderGoToTarget.java
+++ src/no/runsafe/eventengine/entity/PathfinderGoToTarget.java
@@ -2,6 +2,7 @@ package no.runsafe.eventengine.entity;
 
 import net.minecraft.server.v1_7_R1.EntityInsentient;
 import net.minecraft.server.v1_7_R1.PathfinderGoal;
+import no.runsafe.eventengine.EventEngine;
 
 public class PathfinderGoToTarget extends PathfinderGoal
 {
@@ -20,6 +21,8 @@ public class PathfinderGoToTarget extends PathfinderGoal
 	public void c()
 	{
 		EntityGoal goal = EntityDriver.getTarget(entity.getId());
+		EventEngine.Debugger.debugInfo("c() called from path finder!");
+
 		if (goal != null)
 			entity.getNavigation().a(goal.getX(), goal.getY(), goal.getZ(), goal.getSpeed());
 	}
