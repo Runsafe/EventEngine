6a21246dea46ccaf8acc9f71e209a147895df67c
Kruithne <kruithne@gmail.com>
2014-03-14 00:30:58 +0000

    Added some rough-cut debugging.

--- src/no/runsafe/eventengine/handlers/SeatbeltHandler.java
+++ src/no/runsafe/eventengine/handlers/SeatbeltHandler.java
@@ -3,6 +3,7 @@ package no.runsafe.eventengine.handlers;
 import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.entity.ILivingEntity;
 import no.runsafe.framework.api.event.vehicle.IVehicleExit;
+import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.event.vehicle.RunsafeVehicleExitEvent;
 
@@ -11,19 +12,24 @@ import java.util.List;
 
 public class SeatbeltHandler implements IVehicleExit
 {
-	public SeatbeltHandler(IScheduler scheduler)
+	public SeatbeltHandler(IScheduler scheduler, IConsole console)
 	{
 		this.scheduler = scheduler;
+		this.console = console;
 	}
 
 	@Override
 	public void OnVehicleExit(final RunsafeVehicleExitEvent event)
 	{
 		final ILivingEntity rider = event.getExiter();
+		console.logInformation("Vehicle exit event!");
 		if (rider instanceof IPlayer)
 		{
+			console.logInformation("Rider is a player");
 			String playerName = ((IPlayer) rider).getName();
 			if (players.contains(playerName))
+			{
+				console.logInformation("Player is locked.");
 				scheduler.startSyncTask(new Runnable()
 				{
 					@Override
@@ -32,6 +38,7 @@ public class SeatbeltHandler implements IVehicleExit
 						event.getVehicle().setPassenger(rider);
 					}
 				}, 1L);
+			}
 		}
 	}
 
@@ -47,4 +54,5 @@ public class SeatbeltHandler implements IVehicleExit
 
 	private static final List<String> players = new ArrayList<String>(0);
 	private final IScheduler scheduler;
+	private final IConsole console;
 }
