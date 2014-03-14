e4a8eeaafd2808788607fe5b42972fd04253e693
Kruithne <kruithne@gmail.com>
2014-03-14 01:13:03 +0000

    Debugging! Debugging!

--- src/no/runsafe/eventengine/handlers/SeatbeltHandler.java
+++ src/no/runsafe/eventengine/handlers/SeatbeltHandler.java
@@ -3,6 +3,7 @@ package no.runsafe.eventengine.handlers;
 import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.entity.ILivingEntity;
 import no.runsafe.framework.api.event.vehicle.IVehicleExit;
+import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.event.vehicle.RunsafeVehicleExitEvent;
 
@@ -12,22 +13,26 @@ import java.util.UUID;
 
 public class SeatbeltHandler implements IVehicleExit
 {
-	public SeatbeltHandler(IScheduler scheduler)
+	public SeatbeltHandler(IScheduler scheduler, IConsole consoleInstance)
 	{
 		this.scheduler = scheduler;
+		console = consoleInstance;
 	}
 
 	@Override
 	public void OnVehicleExit(final RunsafeVehicleExitEvent event)
 	{
+		console.logInformation("Vehicle exit event detected");
 		final ILivingEntity rider = event.getExiter();
 		if (players.contains(rider.getUniqueId()))
 		{
+			console.logInformation("Player ID is in the lock list");
 			scheduler.startSyncTask(new Runnable()
 			{
 				@Override
 				public void run()
 				{
+					console.logInformation("Remounting");
 					event.getVehicle().setPassenger(rider);
 				}
 			}, 10L);
@@ -36,14 +41,17 @@ public class SeatbeltHandler implements IVehicleExit
 
 	public static void lockPlayer(IPlayer player)
 	{
+		console.logInformation("Locking player!");
 		players.add(player.getUniqueId());
 	}
 
 	public static void unlockPlayer(IPlayer player)
 	{
+		console.logInformation("Unlocking player..");
 		players.remove(player.getUniqueId());
 	}
 
 	private static final List<UUID> players = new ArrayList<UUID>(0);
 	private final IScheduler scheduler;
+	private static IConsole console;
 }
